8cd3e64c26c1590d0806cc58621b8636a77f0904
박진수 <2019040591@hanyang.ac.kr>
2023-03-19 21:03:46 +0900

    first commit

---
+++ mywebsite/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from mywebsite.views import HomeView
+
+# from bookmark.views import BookmarkLV, BookmarkDV
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='home'),
+    path('bookmark/', include('bookmark.urls')),
+    path('blog/', include('blog.urls')),
+
+    # class-based views
+    # path('bookmark/', BookmarkLV.as_view(), name='index'),
+    # path('bookmark/<int:pk>/', BookmarkDV.as_view(), name='detail'),
+]
